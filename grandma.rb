87c15c602e7e4e280c77f3cb870888055025de4c
Jessi Poulson <jessipoulson@hotmail.com>
2020-09-13 07:59:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 def speak_to_grandma(phrase)
- 
-if phrase == "Hi Nana, how are you?"
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase == "Hi!"
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase == "WHAT DID YOU EAT TODAY?"
-  puts "NO, NOT SINCE 1938!"
-elsif phrase == "WHAT?"
-  puts "NO, NOT SINCE 1938!"
-else phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+  if phrase == "Hi Nana, how are you?"
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "Hi!"
+   puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    puts "NO, NOT SINCE 1938!"
+  elsif phrase == "WHAT?"
+    puts "NO, NOT SINCE 1938!"
+  else phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
 end
